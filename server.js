6d137f9e0f04a65e030a7db2fa19bc4567602f4e
zuus1 <azuito@mit.edu>
2024-09-14 20:06:34 +0000

    Adding Hello World

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 const path = require('path');
 // these are some of the libraries you will need
+
+app.get('/', function(req, res) {
+    res.send('<h1> Hello World! </h1>');
+});
+
+app.listen(3000);
+console.log("Running on port 3000");
