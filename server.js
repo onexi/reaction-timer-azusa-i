7e1805eed5d9b7e6e492fa6894045581581e897e
zuus1 <azuito@mit.edu>
2024-09-14 21:06:19 +0000

    Adding !

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Add a Reaction Time</title>
+            <title>Add a Reaction Time!</title>
             <style>
                 Button {
                     background-color: red;
