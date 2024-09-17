6e37ab0164ad60c94a9560f3ba41e5a96697da0f
zuus1 <azuito@mit.edu>
2024-09-17 01:56:37 +0000

    a

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/input', function(req, res){
     const name = escape(req.body.name);
 
     // Add the new user to the array
-    users.push({ name: name });
+    users.push({ name: name, reactionTime: reactionTime });
 
     // If you were to make alphabetical, add here
 
