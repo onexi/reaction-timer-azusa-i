6247bdee6b7bd7e1c631528944d309cf2f20e98c
zuus1 <azuito@mit.edu>
2024-09-17 02:35:29 +0000

    a

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', function(req, res) {
 
 // Handle the form submission
 app.post('/input', function(req, res){
-    const name = escape(req.body.name);
+    const name = req.body.name;
     const reactionTime = req.body.reactionTime;
 
     // Add the new user to the array
